80de0cb0309606ebe0ed215d84e94b901e4dabfc
Christian Gazmuri <christiangazmo@gmail.com>
2021-10-01 15:52:07 -0400

    JUN2010 completed

--- csv/test.js
+++ csv/test.js
@@ -1,15 +1,24 @@
-const HttpsProxyAgent = require('https-proxy-agent');
-const googleTrends = require('google-trends-api');
-let proxyAgent =  new HttpsProxyAgent('http://localhost:8888/');
+var url = require('url');
+var https = require('https');
+var HttpsProxyAgent = require('https-proxy-agent');
 
-let query = {
-    keyword: 'Women\'s march',
-};
+// HTTP/HTTPS proxy to connect to
+var proxy = process.env.http_proxy || 'https://168.63.76.32:3128';
+console.log('using proxy server %j', proxy);
 
-googleTrends.interestOverTime(query)
-.then(function(results){
-  console.log('These proxied results are incredible', results);
-})
-.catch(function(err){
-  console.error('Oh no there was an error, double check your proxy settings', err);
+// HTTPS endpoint for the proxy to connect to
+var endpoint = process.argv[2] || 'https://graph.facebook.com/tootallnate';
+console.log('attempting to GET %j', endpoint);
+var options = url.parse(endpoint);
+
+// create an instance of the `HttpsProxyAgent` class with the proxy server information
+var agent = new HttpsProxyAgent(proxy);
+options.agent = agent;
+
+https.get(options, function (res, err) {
+  if (err) {
+    console.log('Error: ', err);
+  }
+  console.log('"response" event!', res.headers);
+  res.pipe(process.stdout);
 });
