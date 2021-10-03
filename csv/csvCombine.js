ffcd0f025ba507c746264d187b9da7dbe3d8f4d2
Christian Gazmuri <christiangazmo@gmail.com>
2021-10-03 18:06:41 -0400

    Added OCT2010 in 2010.json

--- csv/csvCombine.js
+++ csv/csvCombine.js
@@ -12,7 +12,7 @@ var monthData = {};
 var i = 0;
 var j = 0;
 
-const filename = 'SEP2010.csv';
+const filename = 'OCT2010.csv';
 var year = {
   year: parseInt(filename.slice(3, 7)),
   months: {
--- csv/jsonRead.js
+++ csv/jsonRead.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path')
-const filename = 'SEP2010.json';
+const filename = 'OCT2010.json';
 var year = {
   year: parseInt(filename.slice(3, 7)),
   months: {}
